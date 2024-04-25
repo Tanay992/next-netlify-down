cb4c6b8fe018c87babcee0838f2ef29bbd43247c
Tanay992 <tanay992@ucla.edu>
2024-04-25 12:49:39 +0530

    update favicon

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Shop The Arena</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/STA-logo.png" />
       </Head>
 
       <main>
